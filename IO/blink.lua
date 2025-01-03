5dd4164da304ac23d35b864bbb84746bf79beb21
majorBien <shadow7.dev@gmail.com>
2025-01-03 08:42:29 +0100

    io blink

---
+++ IO/blink.lua
@@ -0,0 +1,20 @@
+channel = 0 -- io channel
+ton = 10 -- time of pulse
+toff = 0 -- delay before the pulse
+blink_num = 1 -- number of blinks
+
+function timeoutHandler()
+	--blink function call
+	dev.out_blink(channel, ton, toff, blink_num)
+end
+
+-- initial function - must be
+function start()
+	dev.set_timeout(1000) -- initial timeout
+end
+
+-- in this function we set timeout of calling timeoutHandler function
+function timeout()
+	timeoutHandler()
+	dev.set_timeout(2000) -- next timeout
+end
